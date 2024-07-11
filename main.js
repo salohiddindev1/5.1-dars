5b5260acb50e09558caf730e765922a87e4eb3c1
salohiddindev1 <169024491+salohiddindev1@users.noreply.github.com>
2024-07-11 09:55:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+let ball = +prompt("hafta kunlarini sonda kiriting!");
+
+switch (ball) {
+  case 1:
+    alert("dushanba");
+    break;
+  case 2:
+    alert("seshanba");
+    break;
+  case 3:
+    alert("chorshanba");
+    break;
+  case 4:
+    alert("payshanba");
+    break;
+  case 5:
+    alert("Juma");
+    break;
+  case 6:
+    alert("shanba");
+    break;
+  case 7:
+    alert("yakshanba");
+    break;
+  default:
+    alert("raqam kiriting!");
+}
+
+// let num = prompt(
+//   "bizda lug'atlar \n olma: \n kiwi: \n anjir: \n mango: \n banan: \n nok: \n shaftoli: \n olxori: \n kakos: \n limon:"
+// );
+// switch (num) {
+//   case "olma":
+//     alert("olma so'zini tarjimasi apple");
+//     break;
+//   case "kiwi":
+//     alert("kiwi sozini tarjimasi kiwi");
+//     break;
+//   case "amjir":
+//     alert("anjir sozini tarjimasi fig");
+//     break;
+//   case "mango":
+//     alert("mango sozini tarjimasi mango");
+//     break;
+//   case "banan":
+//     alert("banana sozini tarjimasi banan");
+//     break;
+//   case "nok":
+//     alert("nok sozini tarjimasi pear");
+//     break;
+//   case "shaftoli":
+//     alert("shaftoli sozini tarjimasi peach");
+//     break;
+//   case "olxori":
+//     alert("olxori sozini tarjimasi plum");
+//     break;
+//   case "kakos":
+//     alert("kakos sozini tarjimasi kakos");
+//     break;
+//   case "limon":
+//     alert("limon sozini tarjimasi lemon");
+//     break;
+//   default:
+//     alert("xali lugatimizda siz kiritgan lugatingiz mavjud emas");
+// }
